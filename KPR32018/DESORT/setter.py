0817f81bd089fbe37e4c44dfd0bdab882164583a
neel <neel12@somaiya.edu>
2018-09-09 14:45:13 +0530

    DSORT setter solution changed

--- KPR32018/DESORT/setter.py
+++ KPR32018/DESORT/setter.py
@@ -3,8 +3,10 @@ t=int(input())
 for _ in range(t):
     n = int(input())
     l = sys.stdin.readline().split()
-    l = list(map(int,l))
-    l.sort(reverse=True)
-    for i in l:
-        print(i, end=" ")
-    print()
+    finlist = []
+    for i in range(n):
+        finlist.append([len(l[i]),l[i]])
+    finlist.sort(reverse=True)
+    for i in finlist:
+        print(i[1], end=" ")
+    print() 
