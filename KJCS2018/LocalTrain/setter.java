513fc428a496787e47231b826a0c6ca5f03b5d3c
Chirag Shetty <nurdtechie98@gmail.com>
2018-10-01 01:40:16 +0530

    Update setter.java

--- KJCS2018/LocalTrain/setter.java
+++ KJCS2018/LocalTrain/setter.java
@@ -1,5 +1,5 @@
 import java.util.*;
-class Train{
+class Setter{
     public static void main(String args[]){
         Scanner sc=new Scanner(System.in);
         int t=sc.nextInt();
@@ -8,7 +8,8 @@ class Train{
         while(t-->0){
             long n=sc.nextLong(),m=sc.nextLong();
             long limit=(long)1e18;
-            assert n>=1 && n<=limit && m>=1 && m<=10 ;
+            assert n>=1l && n<=limit; 
+            assert m>=1l && m<=10l ;
     
             ArrayList<Long> v=new ArrayList<>();
             for(int i=0;i<m;++i) {
@@ -18,8 +19,6 @@ class Train{
             }
             long ans=inclu_exclu(v,n);
             System.out.println(n-ans); 
-    
-    
     
         }
 
@@ -32,25 +31,18 @@ static long inclu_exclu(ArrayList<Long> v,long num){
                for(int k=0;k<N;k++){ 
                 if((i&(1<<k))>=1){
                     ct=lcm(ct,v.get(k));
-                    if(ct<0) {
-                        //System.out.println("-v");
-                        break;
-                        
-                    }
-                    
                 }
                }
-               if(ct>=0){
+               
                if((Integer.bitCount(i))%2==1) sum+=(num/ct);
                else  sum-=(num/ct);
-               }
+               
            }
            return sum;
        }
        static long lcm(long x,long y){
                 long result = x*y;
-                if( x != 0 && result/x != y) return -1l;
-                long g=gcd(x,y);
+                 long g=gcd(x,y);
                 long ans=(x*y)/g;
                 return ans;
        }
